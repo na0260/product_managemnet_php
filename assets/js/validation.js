951c06183f5f7980c4520e4ec269e746eda1d621
na0260 <samjr.008@gmail.com>
2024-09-04 20:43:01 +0600

    js validation added

---
+++ assets/js/validation.js
@@ -0,0 +1,25 @@
+// js/validation.js
+
+function validateRegistrationForm() {
+    var username = document.forms["registrationForm"]["username"].value;
+    var email = document.forms["registrationForm"]["email"].value;
+    var password = document.forms["registrationForm"]["password"].value;
+    var confirmPassword = document.forms["registrationForm"]["confirm_password"].value;
+
+    if (username == "" || email == "" || password == "" || confirmPassword == "") {
+        alert("All fields must be filled out.");
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+
+    return true;
+}
