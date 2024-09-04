3c3b12662f622f0f88314adc034a9721aca38c92
na0260 <samjr.008@gmail.com>
2024-09-04 20:45:08 +0600

    home page added

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Management System</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 <div class="container">
     <h1>Welcome to the Product Management System</h1>
     <p>Please log in or register to manage products.</p>
     <div class="buttons">
-        <a href="login.php" class="btn">Login</a>
-        <a href="register.php" class="btn">Register</a>
+        <a href="./login.php" class="btn">Login</a>
+        <a href="./register.php" class="btn">Register</a>
     </div>
 </div>
 </body>
